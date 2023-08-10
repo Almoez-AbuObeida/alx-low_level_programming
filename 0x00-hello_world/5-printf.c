6300a9597bec8084c3caf07a0ae1df437ac43c92
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-10 20:03:43 +0200

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main - The main function
+(*
+*Description: This function prints a text using printf.)
+*Return: return 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
