1c07ef80091e41a0bcaaeffe02c6efa2e5303bd5
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-10 19:25:13 +0200

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-/*The main function*/
+/**
+*main functoin - A function prints a text using puts
+*@text: the text variable
+
+*/
 int main(void)
 {
 	/*A string of chars to output*/
