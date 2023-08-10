21917f7327b73c8fbd64b277e7ed7853e679d998
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-10 19:48:44 +0200

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
 /**
 *main functoin - A function prints a text using puts
 (*
-*Description: This function is going to print a text)
-*Reiturn: 0
+*Description: This function is going to print a text using puts)
+*Reiturn: This program is going to return 0
 */
 int main(void)
 {
