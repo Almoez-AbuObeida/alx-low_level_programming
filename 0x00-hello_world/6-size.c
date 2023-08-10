824124c0c4adbb5938d0c99aa284078c5a88aa98
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-10 21:35:04 +0200

    hello

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,7 +17,7 @@ int main(void)
 	printf("size of  a char: %zu byte(s)\n", sizeof(c));
 	printf("size of  a int: %zu byte(s)\n", sizeof(i));
 	printf("size of  a long int: %zu byte(s)\n", sizeof(l));
-	printf("size of  a long long int: %zu byte(s)\n", sizeof(o);
+	printf("size of  a long long int: %zu byte(s)\n", sizeof(o));
 	printf("size of  a float: %zu byte(s)\n", sizeof(f));
 	return (0);
 }
