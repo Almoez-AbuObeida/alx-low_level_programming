9eec8a1978f637e5887afaca38e23c06c6fd1d19
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-10 22:54:55 +0200

    Done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*main - main function
+(*
+*Description: This function is going to return 0)
+*Return: return 1
+*/
+int main(void)
+{
+	fputchar(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
