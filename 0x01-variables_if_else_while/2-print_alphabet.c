dec53d63e814eae799d71e54c6e2a3aaa16e59a7
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-11 13:28:42 +0200

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+*main - main function
+(*
+*Description: this function will print the alphabet.)
+*Return: 0
+*/
+int main(void)
+{
+	char n = 'a';
+
+	while (n <= z)
+	{
+		printf("%c", n);
+		n++;
+	}
+	printf("\n");
+
+	return (0);
+}
