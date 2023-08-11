6cdb2fae64e8e3dd5e13145ba61e2f3f49703ece
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-11 23:09:43 +0200

    Four digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,8 +13,8 @@ int main(void)
 	int i;
 
 	for (i = 0; i <= 99; i++)
-       {
-	   	int j;
+     {
+	  	int j;
 
            for (j = i + 1; j <= 99; j++)
            {
@@ -23,14 +23,13 @@ int main(void)
                putchar(' ');
                putchar((j / 10) + '0');
                putchar((j % 10) + '0');
-
                if (i < 98 || j < 99)
                {
                    putchar(',');
                    putchar(' ');
-               }           
+               }
 			}
-       }	
+      }
 	putchar('\n');
 
 	return (0);
