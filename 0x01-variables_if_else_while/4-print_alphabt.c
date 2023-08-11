8fac8647b7289e0890aad6c35ff93853ec8eb3c1
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-11 13:55:15 +0200

    All except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+*main - main function
+(*
+*Description: This function will print all the alphaber 
+*letters except e and q)
+*Return: 0
+*/
+int main(void)
+{
+	char n = 'a';
+
+	while (a <= 'z' && a != 'q' && a != 'e')
+	{
+		putchar(n);
+		n++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
