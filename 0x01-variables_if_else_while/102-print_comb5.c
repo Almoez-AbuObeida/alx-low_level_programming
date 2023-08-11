fe94c923e99f82206ec9149b03cf09942509892c
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-11 22:14:53 +0200

    Four digits

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+*main - main function
+(*
+*Description: this function is going to print every number in
+*three digits while the first is less than the second and the second
+*is less than and the third)
+*Return: This function will return 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		int j;
+
+		for (j = 0; j < 10; j++)
+		{
+			int k;
+
+			for (k = 0; k < 10; k++)
+			{
+				int l;
+				
+				for (l = k + 1; l < 10; l++)
+				{
+					putchar(i + 48);
+					putchar(j + 48);
+					putchar(' ');
+					putchar(k + 48);
+					putchar(l + 40);
+					if (i < 9 || j < 9 || k < 9 || l < 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
