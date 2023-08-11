b60081864d838bc8e338a4817a0ba5093a3e6fe1
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-11 15:09:53 +0200

    Mandatory done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+*main - main funcion
+(*
+*Description: This function will print numbers from 0 to 9
+*separated by ,
+*Return: 0
+*/
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		putchar(48 + n);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
