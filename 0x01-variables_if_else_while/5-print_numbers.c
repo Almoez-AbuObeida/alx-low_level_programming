e099a2e453dfefe1cd862455483c9e0db753d972
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-11 14:16:59 +0200

    From 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - main function
+(*
+*Description: this function will print numbers from 0 to 9)
+*Retrun: 0
+*/
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+}
