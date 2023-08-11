1cab3263c123c1b3bb99dc60133bf70eecdc9c02
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-11 14:58:54 +0200

    Base 16 numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+*main - main function
+*Description: This function will print the base 16 numbers.)
+*Return: 0
+*/
+int main(void)
+{
+	char i = '0';
+	char c = 'a';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	while(c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+}
