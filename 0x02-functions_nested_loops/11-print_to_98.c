fb0b7b7d816ffd6058ec4bd07bd4650f942c73aa
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-19 00:47:20 +0200

    To 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,31 +1,42 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-* print_to_98 - print form any number to 98
-* @n: the number to count from
-* Return: all numbers from n to 98
+* times_table - print times table
+(*
+* Return: nothing
 */
 
 void print_to_98(int n)
 {
+	int i, j;
+
 	if (n <= 98)
 	{
-		while (n <= 98)
+		for (i = n; i <= 98; i++)
 		{
-			_putchar((int) n);
-			_putchar(',');
-			_putchar(' ');
-			n++;
+			if (i != 98)
+			{
+				printf("%d, ", i);
+			}
+			else if (i == 98)
+			{
+				printf("%d\n", i);
+			}
 		}
 	}
 	else if (n >= 98)
 	{
-		while (n >= 98)
+		for (j = n; j >= 98; j--)
 		{
-			_putchar((int) n);
-			_putchar(',');
-			_putchar(' ');
-			n--;
+			if (j != 98)
+			{
+				printf("%d, ", j);
+			}
+			else if (j == 98)
+			{
+				printf("%d\n", j);
+			}
 		}
 	}
-}
+}	
