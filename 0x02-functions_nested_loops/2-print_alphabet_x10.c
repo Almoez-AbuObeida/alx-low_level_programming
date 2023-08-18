1b7f54a4694137b172a82d4bfb66a17b3c4a4dbb
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-18 14:30:41 +0200

    x10 alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,9 @@
 void print_alphabet_x10(void)
 {
 	char c = 'a';
-	int counter = 0;
+	int i;
 
-	while (counter < 10)
+	for (i = 0; i < 10; i++)
 	{
 		while (c <= 'z')
 		{
@@ -18,6 +18,5 @@ void print_alphabet_x10(void)
 			c++;
 		}
 		_putchar('\n');
-		counter++;
 	}
 }
