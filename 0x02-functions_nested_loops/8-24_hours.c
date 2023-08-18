1d897a95b236567b517d91fd8f367d6e5f20b6eb
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-18 16:22:45 +0200

    time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+* jack_bauer - print all minuts
+(*
+* Return: nothing
+*/
+
+void jack_bauer(void);
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			int k, l;
+
+			_putchar(i);
+			_putchar(j);
+			_putchar(':');
+
+			for (k = 0; k < 60; k++)
+			{
+				for (l = 0; l < 60; l++)
+				{
+					_putchar(k);
+					_putchar(l);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+	
+}
