bbc1f0baa3e9379086fdcbc48400b546836c6a9f
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-18 16:49:22 +0200

    time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,23 +12,14 @@ void jack_bauer(void)
 
 	for (i = 0; i < 3; i++)
 	{
-		for (j = 0; j < 4; j++)
+		for (j = 0; j < 60; j++)
 		{
-			int k, l;
-
-			_putchar(i +48);
-			_putchar(j + 48);
+			_putchar(i / 10 + 48);
+			_putchar(i % 10 + 48);
 			_putchar(':');
-
-			for (k = 0; k < 60; k++)
-			{
-				for (l = 0; l < 60; l++)
-				{
-					_putchar(k + 48);
-					_putchar(l + 48);
-					_putchar('\n');
-				}
-			}
+			_putchar(j / 10 + 48);
+			_putchar(j % 10 + 48);
+			_putchar('\n');
 		}
 	}
 	
