b4ade06210a816b2f47c4468da85a62eb090b867
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-18 14:54:46 +0200

    Is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _isalpha - check if is alpha or not
+(*
+* @c: a character to check
+* Return: 1 if char and 0 if not
+*/
+
+int _isalpha(int c);
+{
+	if (isalpha((char) c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
