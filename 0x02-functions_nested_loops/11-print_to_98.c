2eedf4c6255865a65907e207f56c6366491f08b0
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-18 23:53:38 +0200

    To 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* print_to_98 - print form any number to 98
+* @n: the number to count from
+* Return: all numbers from n to 98
+*/
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n <= 98)
+		{
+			_putchar((int) n)
+			_putchar(',');
+			_putchar(' ');
+			n++;
+		}
+	}
+	else if (n >= 98)
+	{
+		while (n >= 98)
+		{
+			_putchar((int) n);
+			_putchar(',');
+			_putchar(' ');
+			n--;
+		}
+	}
+}
