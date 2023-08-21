995f86b02aeaf3e5882d968fa55fb02c671a5f2f
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-21 23:29:34 +0200

    Every other

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,15 @@
 
 void puts2(char *str)
 {
-	int i = 0;
+	int i, j;
 
 	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
-		i += 2;
+		i++;
+	}
+	for (j = 0; j < i; j++)
+	{
+		_putchar(str[j]);
 	}
 	_putchar('\n');
 }
