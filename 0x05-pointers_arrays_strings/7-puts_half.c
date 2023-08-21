e33c426eeca11b08aa73a4aca7cd28e92fd5c2eb
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-21 23:56:48 +0200

    Print half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -18,7 +18,7 @@ void puts_half(char *str)
 	{
 		int n = i / 2;
 
-		while (str[n] != '\0')
+		while (n < (i - 1))
 		{
 			_putchar(str[n]);
 			n++;
@@ -28,7 +28,7 @@ void puts_half(char *str)
 	{
 		int n = (i - 1) / 2;
 
-		while (str[n] != '\0')
+		while (n < (i - 1))
 		{
 			_putchar(str[n]);
 			n++;
