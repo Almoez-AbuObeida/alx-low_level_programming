4a79e7e45670e98e4f0fdf08d1a058b0861d39fd
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-21 20:01:42 +0200

    Reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
 * reset_to_98 - a function that takes a pointer to
 * an int as parameter and updates the value it points to to 98
-* @*n: The Dereference of a variable n
+* @n: The Dereference of a variable n
 * Return: Nothing.
 */
 
