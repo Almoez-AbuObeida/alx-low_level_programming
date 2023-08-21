c17602a00b1a76c730ecf35c41e0f085a90158bc
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-21 22:44:58 +0200

    Reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -15,16 +15,14 @@ void rev_string(char *s)
 	{
 		i++;
 	}
-	for (k = i; k > 0; k--)
-	{
-		for (j = 0; j < i; j++)
-		{
-			char a = s[j];
-			char b = s[j + 1];
 
-			s[j] = b;
-			s[j + 1] = a;
-		}
+	for (j = 0; j < i; j++)
+	{
+		char a = s[j];
+		char b = s[j + 1];
+
+		s[j] = b;
+		s[j + 1] = a;
 	}
 	_putchar('\n');
 }
