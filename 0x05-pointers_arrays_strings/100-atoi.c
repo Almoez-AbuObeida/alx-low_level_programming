aefac6b2fca39dba8d7f0519c7ea0bcb2cfdb32f
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-22 02:11:39 +0200

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,22 +9,19 @@ int _atoi(char *s)
 {
 	int i = 0;
 	int j = 0;
+	int k = 0;
 
 	while (s[i] != '\0')
 	{
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			j = j * 10 + s[i] - '0';
-		 	if (s[i] == '-')
-			{
-				j = -j;
-			}
-			if (s[0] == '-')
-			{
-				j = j * -1;
-			}
 		}
 		i++;
+	}
+	while (s[k] == '-' && s[k] != '\0')
+	{
+		j = j * -1;
 	}
 
 	return (j);
