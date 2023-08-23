83dd060096673d08797bd599b7c2f4fd20bdc65d
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-23 18:26:55 +0200

    Strcat to a mount

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* _strcat - a function that concatenates two strings.
+* @dest: a string
+* @src: a stirng
+* Return: The tow strings @dest and @src.
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	dest[i] = ' ';
+	while (j < n)
+	{
+		char tmp = src[j];
+
+		dest[i + j] = tmp;
+		j++;
+	}
+	dest[i + j] = '\0';
+
+	return (dest);
+}
