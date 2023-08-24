8140c020ec9fe745fb29150743bf7a3e69ea9e68
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-24 12:27:25 +0200

    cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,9 +12,9 @@ char *cap_string(char *s)
 
 	while (s[i] != '\0')
 	{
-		if (i == 0)
+		while (s[i] < 'a' && s[i] > 'z')
 		{
-			s[i] -= 32;
+			i++;
 		}
 		if (s[i - 1] == ' ' ||
 			s[i - 1] == '\n' ||
@@ -28,11 +28,9 @@ char *cap_string(char *s)
 			s[i - 1] == '(' ||
 			s[i - 1] == ')' ||
 			s[i - 1] == '}' ||
-			s[i - 1] == '{')
-			{
-				if (s[i] >= 'a' && s[i] <= 'z')
-					s[i] -= 32;
-			}
+			s[i - 1] == '{' ||
+			i == 0)
+				s[i] -= s[i];
 		i++;
 	}
 
