552c0843aca3cf6c889d23a32feb179c76128476
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-24 14:41:15 +0200

    Rot 13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* rot13 - a function that encodes a string using rot13.
+* @s: string
+* Return: @s.
+*/
+
+char *rot13(char *s)
+{
+	int i, j;
+	char *h1 = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+	char *h2 = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; h1[j] != '\0'; j++)
+		{
+			if (s[i] == h1[j])
+			{
+				s[i] = h2[j];
+			}
+		}
+	}
+
+	return (s);
+}
