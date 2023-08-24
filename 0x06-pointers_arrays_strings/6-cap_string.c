fd72dd5ab0b931537b0c75ebd97317e10f7260a4
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-24 12:28:17 +0200

    cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,9 +13,7 @@ char *cap_string(char *s)
 	while (s[i] != '\0')
 	{
 		while (s[i] < 'a' && s[i] > 'z')
-		{
 			i++;
-		}
 		if (s[i - 1] == ' ' ||
 			s[i - 1] == '\n' ||
 			s[i - 1] == '\t' ||
