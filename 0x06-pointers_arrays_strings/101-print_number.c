ba916e4cdfcbfd1f5f756a83715afe4c0f32477c
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-24 19:13:40 +0200

    Print int

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,5 +7,22 @@
 */
 void print_number(int n)
 {
-	_putchar(n + 48);
+	int power = 10;
+	int len = 0;
+	int i = 1;
+
+	if (n < 0)
+		_putchar('-');
+
+	while (n / power != 0)
+	{
+		len++;
+		power *= 10;
+	}
+
+	while (i < len)
+	{
+		_putchar(n / (10 * (len - i)) + 48);
+		i++;
+	}
 }
