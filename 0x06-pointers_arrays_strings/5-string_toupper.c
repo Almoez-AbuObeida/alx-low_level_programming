6914bd2ce2ef9463bf96192e41e5f2a1d71f48c8
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-24 10:23:16 +0200

    strcmp

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* string_toupper - a function that changes all lowercase
+* letters of a string to uppercase.
+* @s: string
+* Return: @s.
+*/
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+
+	return (s);
+}
