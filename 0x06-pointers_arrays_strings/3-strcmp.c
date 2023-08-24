2d41a28c05e1ba3925cd42d2a8bd526a0d2cec4b
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-24 09:10:24 +0200

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,15 +11,17 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
+	int j = 0;
 
 	while (s1[i] != '\0')
 	{
-		if (s1[i] != s2[i])
+		if (s1[i] != s2[j])
 		{
-			return (s1[i] - s2[i]);
 			break;
 		}
+		i++;
+		j++;
 	}
 
-	return (0);
+	return (s1[i - 1] - s2[j - 1]);
 }
