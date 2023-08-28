0cd0ec5ecd65106b70cc579332ef9090cc415685
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-28 19:43:03 +0200

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,6 +7,7 @@
 * @accept: string cosist of bytes should be counted.
 * Return: s.
 */
+char *_strchr(char *s, char c);
 
 char *_strpbrk(char *s, char *accept)
 {
@@ -15,7 +16,7 @@ char *_strpbrk(char *s, char *accept)
 
 	while (*s)
 	{
-		if (strchr(accept, *s))
+		if (_strchr(accept, *s))
 		{
 			return s;
 		}
@@ -26,4 +27,30 @@ char *_strpbrk(char *s, char *accept)
 	}
 
 	return NULL;
+}
+
+/**
+* _strchr - A function that locates a character in a string.
+* @s: A string to locat characters.
+* @c: A char to locat.
+* Return: a pointer if found or NULL if not.
+*/
+
+char *_strchr(char *s, char c)
+{
+	char *p = NULL;
+	/*handling null pointer*/
+	if (s != NULL)
+	{
+		while (*s++)
+		{
+			if (*s == c)
+			{
+				p = s;
+				break;
+			}
+		}
+	}
+
+	return (p);
 }
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 
 #include <unistd.h>
-#include <string.h>
 
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
