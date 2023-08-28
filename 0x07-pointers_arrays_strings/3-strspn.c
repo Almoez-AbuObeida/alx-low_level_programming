0f030acbc904bbf4d30aae3beae059461ecf5daa
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-28 18:28:37 +0200

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,18 +10,21 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int c = 0;
+	int i = 0;
 
 	if ((s == NULL) || (accept == NULL))
 		return (c);
 
-	while (*s++ != '\0')
+	while (s[i] != '\0')
 	{
 		while (*accept++ != '\0')
 		{
 			if (*s == *accept)
 				c++;
 
+			j++;
 		}
+		i++;
 	}
 
 	return (c);
