7421db8292062279b2930023f3661d7ad5e4e688
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-08-30 18:51:22 +0200

    sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _sqrt_recursion - a function that returns the natural
+* square root of a number.
+* @n: The number to get the squar root for.
+* Return: The squar root.
+*/
+
+int _sqrt_recursion(int n)
+{
+	int guess = 0;
+
+	if (n == 0)
+		return (0);
+	if (guess < n)
+	{
+		if (n / (guess * guess) == 1)
+			return (guess);
+	}
+
+	guess++;
+
+	return (-1)
+}
