f54dcbd05f729bbfea563f5b742b2b7f67935bcf
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-09-02 00:06:16 +0200

    print argc

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
 * main - print the number of arguments.
@@ -12,13 +13,13 @@ int main(int argc, char *argv[])
 	int i;
 
 	if (argc == 1)
-		_putchar('0');
+		printf("%d\n", argc - 1);
 	else
 	{
 		for (i = 0; *argv; i++, argv++)
 			;
 
-		_putchar((i - 1) + '0');
+		printf("%d\n", i - 1);
 	}
 
 	return (0);
