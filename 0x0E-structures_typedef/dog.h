2e5b8c8e44f0530ee612d7d55d73837843564161
Almoiz-Abu-Obaida <Eng.almoizsaeed@gmail.com>
2023-09-14 22:10:15 +0200

    Adding dog lib

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
+
+/**
+ * struct dog - to store information about dogs.
+ * @name: the name of the dog.
+ * @age: the age of the dog.
+ * @owner: the owner of the dog.
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
 #endif
